3b2a9950104a228bf3e035fb44cc00ee8542f45e
Kene Arionye <kenechiaugustine@gmail.com>
2026-06-07 16:38:46 +0100

    feat: enhance layout for Admin, Client, and User components with smooth scrolling and viewport adjustments

--- src/layouts/ClientLayout/index.tsx
+++ src/layouts/ClientLayout/index.tsx
@@ -183,7 +183,7 @@ export default function ClientLayout() {
                 </header>
 
                 {/* Sub-page Render Box */}
-                <div className="flex-grow overflow-y-auto p-4 sm:p-6 md:p-8">
+                <div className="flex-grow overflow-y-auto p-4 sm:p-6 md:p-8 scroll-smooth">
                     <Outlet />
                 </div>
             </main>
